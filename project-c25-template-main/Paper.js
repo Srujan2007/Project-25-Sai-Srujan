47d2d8febb2ee382cf7033a8c12d901349be0605
Srujan2007 <82515559+Srujan2007@users.noreply.github.com>
2021-07-09 18:29:02 +0530

    Add files via upload

---
+++ project-c25-template-main/Paper.js
@@ -0,0 +1,35 @@
+class Paper{
+	constructor(x,y,r)
+	{
+		var options={
+			restitution:0.3,
+			friction:0,
+			density:1.2
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+         this.image = loadImage("paper.png");
+		this.body=Bodies.circle(x,y, this.r/3, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;
+			push()
+			translate(paperpos.x, paperpos.y);
+			
+			//rectMode(CENTER)
+			//strokeWeight(4);
+			//fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			//ellipse(0,0,this.r*2, this.r);
+			pop()
+			
+	}
+
+}
